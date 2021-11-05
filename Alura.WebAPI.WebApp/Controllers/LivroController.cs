8112bdb08ee6fe5ce6c1ba5e8d998f7a36b166c5
LucasDev13 <lucaspontes140@gmail.com>
2021-11-05 16:38:27 -0300

    Estrutura do projeto criando a consulta da api com httpClient.

---
+++ Alura.WebAPI.WebApp/Controllers/LivroController.cs
@@ -0,0 +1,105 @@
+﻿using System.Linq;
+using Alura.ListaLeitura.Persistencia;
+using Alura.ListaLeitura.Modelos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.WebApp.Controllers
+{
+    [Authorize]
+    public class LivroController : Controller
+    {
+        private readonly IRepository<Livro> _repo;
+
+        public LivroController(IRepository<Livro> repository)
+        {
+            _repo = repository;
+        }
+
+        [HttpGet]
+        public IActionResult Novo()
+        {
+            return View(new LivroUpload());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Novo(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                _repo.Incluir(model.ToLivro());
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ImagemCapa(int id)
+        {
+            var api = new LivroApiclient();
+            byte[] img = await resposta.Content.ReadAsByteArrayAsync();
+            if (img != null)
+            {
+                return File(img, "image/png");
+            }
+            return File("~/images/capas/capa-vazia.png", "image/png");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Detalhes(int id)
+        {
+            //http://localhost:5000/api/livros/{id}
+            //http://localhost:5000/api/listasLeituras/{tipo}
+            //http://localhost:5000/api/livros/{id}/capa
+            
+            var httpClient = new HttpClient();
+            httpClient.BaseAddress = new System.Uri("http://localhost:5000/api/");
+            HttpResponseMessage resposta = await httpClient.GetAsync($"livros/{id}");
+            resposta.EnsureSuccessStatusCode();
+
+
+            var model = await resposta.Content.ReadAsAsync<LivroApi>();
+            if (model == null)
+            {
+                return NotFound();
+            }
+            return View(model.ToUpload());
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Detalhes(LivroUpload model)
+        {
+            if (ModelState.IsValid)
+            {
+                var livro = model.ToLivro();
+                if (model.Capa == null)
+                {
+                    livro.ImagemCapa = _repo.All
+                        .Where(l => l.Id == livro.Id)
+                        .Select(l => l.ImagemCapa)
+                        .FirstOrDefault();
+                }
+                _repo.Alterar(livro);
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Remover(int id)
+        {
+            var model = _repo.Find(id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            _repo.Excluir(model);
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
---
+++ Alura.WebAPI.WebApp/HttpClients/LivroApiClient.cs
@@ -0,0 +1,36 @@
+﻿using Alura.ListaLeitura.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.HttpClients
+{
+    public class LivroApiClient
+    {
+
+        private readonly HttpClient _httpClient;
+
+        public LivroApiClient()
+        {
+            _httpClient = new HttpClient();
+            _httpClient.BaseAddress = new System.Uri("http://localhost:5000/api/");
+        }
+
+        public async Task<byte[]> GetCapaLivroAsync(int id)
+        {
+            
+            HttpResponseMessage resposta = await _httpClient.GetAsync($"livros/{id}/capa");
+            resposta.EnsureSuccessStatusCode();
+            return await resposta.Content.ReadAsByteArrayAsync();
+        }
+
+        public async Task<LivroApi> GetLivroApiAsync(int id)
+        {
+            HttpResponseMessage resposta = await _httpClient.GetAsync($"livros/{id}");
+            resposta.EnsureSuccessStatusCode();
+            return await resposta.Content.ReadAsAsync<LivroApi>();
+        }
+    }
+}
